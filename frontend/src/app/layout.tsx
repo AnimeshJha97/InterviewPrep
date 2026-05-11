078dab01acc4d0aec9e44eb02f6b0a4828b46be0
AnimeshJha97 <ani557ssj10@gmail.com>
2026-05-12 05:12:37 +0530

    added icon

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,12 +1,17 @@
 import type { Metadata } from "next";
 
 import { BRAND } from "@/data/brand";
+import favicon from "@/data/assets/favicon.ico";
 
 import "./globals.css";
 
 export const metadata: Metadata = {
   title: BRAND.fullName,
   description: BRAND.shortDescription,
+  icons: {
+    icon: favicon.src,
+    shortcut: favicon.src,
+  },
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
